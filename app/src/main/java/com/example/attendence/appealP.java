9f4e6ac9d7396619bb9789a8d0866519a114205e
k-jayeoneee <137jayeon0322@naver.com>
2025-08-04 03:16:20 +0900

    출결 현황 버튼 기능 추가

--- app/src/main/java/com/example/attendence/appealP.java
+++ app/src/main/java/com/example/attendence/appealP.java
@@ -1,12 +1,18 @@
 package com.example.attendence;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import android.widget.LinearLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +65,50 @@ public class appealP extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appeal_p, container, false);
+        View view = inflater.inflate(R.layout.fragment_appeal_p, container, false);
+        Button button_attend_p = view.findViewById(R.id.student_status_button);
+        button_attend_p.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "출결 상태가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+        });
+        return  view;
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button appealBtn = view.findViewById(R.id.attend_status_appeal);
+        LinearLayout appealOptions = view.findViewById(R.id.appeal_options);
+        TextView option1 = view.findViewById(R.id.option1);
+        TextView option2 = view.findViewById(R.id.option2);
+        TextView option3 = view.findViewById(R.id.option3);
+        LinearLayout layoutItem= view.findViewById(R.id.student_appeal_item);
+        TextView reasonText = view.findViewById(R.id.reason_text);
+
+        appealBtn.setOnClickListener(v -> {
+            if (appealOptions.getVisibility() == View.GONE) {
+                appealOptions.setVisibility(View.VISIBLE);
+            } else {
+                appealOptions.setVisibility(View.GONE);
+            }
+        });
+
+        View.OnClickListener optionClickListener = v -> {
+            TextView option = (TextView) v;
+            appealBtn.setText(option.getText());
+            appealOptions.setVisibility(View.GONE);
+        };
+        option1.setOnClickListener(optionClickListener);
+        option2.setOnClickListener(optionClickListener);
+        option3.setOnClickListener(optionClickListener);
+
+        layoutItem.setOnClickListener(v -> {
+            if (reasonText.getVisibility() == View.GONE){
+                reasonText.setVisibility(View.VISIBLE);
+            } else {
+                reasonText.setVisibility(View.GONE);
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/attendence/appealS.java
+++ app/src/main/java/com/example/attendence/appealS.java
@@ -2,11 +2,17 @@ package com.example.attendence;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +65,29 @@ public class appealS extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appeal_s, container, false);
+        View view = inflater.inflate(R.layout.fragment_appeal_s, container, false);
+        Button button_attend_p = view.findViewById(R.id.student_appeal_button);
+        button_attend_p.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "출결 상태가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+        });
+        return  view;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button appealStatusBtn = view.findViewById(R.id.attend_status_appeal);
+        LinearLayout appealEditBox = view.findViewById(R.id.student_appeal_editbox);
+
+
+        appealStatusBtn.setOnClickListener(v -> {
+            if (appealEditBox.getVisibility() == View.GONE) {
+                appealEditBox.setVisibility(View.VISIBLE);
+            } else {
+                appealEditBox.setVisibility(View.GONE);
+            }
+        });
+
+
     }
 }
